fa94b353b0b59d20b78c295c75b142d57a9799d7
Julio Nuñez <julionr14@gmail.com>
2021-09-25 14:22:08 -0600

    preparando proyecto

---
+++ js/componentes-bootstrap.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".carousel").carousel({
+        interval:1000
+    });
+})
+
+document.getElementById("activar-alert").addEventListener("click", function(){
+    $(#alert)
+})
